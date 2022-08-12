56aea7e519f9362a19d533362bf4f43b743f78b5
Michael Wang <41971562+devmwang@users.noreply.github.com>
2022-08-11 18:35:45 -0700

    Created Side Links

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,26 @@
 import type { NextPage } from "next";
 import Head from "next/head";
 
-type TechnologyCardProps = {
-    name: string;
+import React, { useState,  useEffect } from "react";
+import { GitHub, Linkedin, Mail } from "react-feather";
+
+type LinkDetails = {
     description: string;
-    documentation: string;
-};
+    link: string;
+    iconComponent: any;
+}
+
+interface FixedLinksProps {
+    linkList: LinkDetails[];
+    revealDelay: number;
+    inBetweenDelay: number;
+}
+
+const links = [
+    { description: "GitHub", link: "https://github.com/devmwang/", iconComponent: <GitHub /> },
+    { description: "LinkedIn", link: "https://linkedin.com/in/devmwang/", iconComponent: <Linkedin /> },
+    { description: "Email", link: "mailto:devmwang@icloud.com", iconComponent: <Mail /> },
+];
 
 const Home: NextPage = () => (
     <>
@@ -15,8 +30,20 @@ const Home: NextPage = () => (
             <link rel="icon" href="/favicon.svg" />
         </Head>
 
-        <main className="min-h-screen bg-background flex flex-col font-sans">
-            <div className="h-screen justify-center items-center flex flex-col text-center">
+        <main className="min-h-screen flex flex-col font-sans">
+            {/* Fixed Links */}
+            <FixedLinks linkList={links} revealDelay={100} inBetweenDelay={200} />
+
+            {/* Main Content */}
+            <div className="h-screen bg-background justify-center items-center flex flex-col text-center">
+                <h1 className="text-5xl md:text-8xl leading-normal font-semibold text-white align-middle">
+                    {"Hi, I'm Michael"}
+                </h1>
+                <p className="text-lg md:text-4xl pt-1 md:pt-8 leading-normal font-normal text-white align-middle">
+                    {"I'm a student pursuing Computer Science."}
+                </p>
+            </div>
+            <div className="h-screen bg-dark justify-center items-center flex flex-col text-center">
                 <h1 className="text-5xl md:text-8xl leading-normal font-semibold text-white align-middle">
                     {"Hi, I'm Michael"}
                 </h1>
@@ -28,25 +55,52 @@ const Home: NextPage = () => (
     </>
 );
 
-const TechnologyCard = ({
-    name,
-    description,
-    documentation,
-}: TechnologyCardProps) => {
+const FixedLinks = ({
+    linkList,
+    revealDelay,
+    inBetweenDelay,
+}: FixedLinksProps) => {
+    const [isSideLinksVisible, setSideLinksVisible] = useState(new Array(linkList.length).fill(false));
+
+    // Runs on component mount
+    useEffect(() => {
+        // For each link, reveal after specified delay
+        for (let i=0; i<linkList.length; i++) {
+            const linkRevealDelay = revealDelay + (i * inBetweenDelay);
+
+            setTimeout(() => {
+                setSideLinksVisible((currentValues: any) => {
+                    return currentValues.map((value: boolean, index: number) => {
+                        return index == i ? true : value;
+                    })
+                })
+            }, linkRevealDelay)
+        }
+    }, []);
+
+    // Link List Items
+    const sideLinkItems = linkList.map((linkItem, index) => {
+        const { description, link, iconComponent } = linkItem;
+        
+        // Pass size prop to icon component
+        const sizedIconComponent = React.cloneElement(iconComponent, { size: '2rem' });
+
+        return (
+            <li key={link} className={isSideLinksVisible[index] ? "py-3 transition-all ease-in-out duration-500 delay-[0ms] opacity-100 translate-y-0" : "py-2.5 opacity-0 translate-y-8"} >
+                <a href={link} target="_blank" rel="noopener noreferrer" aria-label={description} className="text-bright h-8 opacity-60">
+                    {sizedIconComponent}
+                </a>
+            </li>
+        )
+    })
+
     return (
-        <section className="flex flex-col justify-center p-6 duration-500 border-2 border-gray-500 rounded shadow-xl motion-safe:hover:scale-105">
-            <h2 className="text-lg text-gray-700">{name}</h2>
-            <p className="text-sm text-gray-600">{description}</p>
-            <a
-            className="mt-3 text-sm underline text-violet-500 decoration-dotted underline-offset-2"
-            href={documentation}
-            target="_blank"
-            rel="noreferrer"
-            >
-                Documentation
-            </a>
-        </section>
-    );
-};
+        <div className="fixed w-8 bottom-10 left-12">
+            <ul className="list-none">
+                {sideLinkItems}
+            </ul>
+        </div>
+    )
+}
 
 export default Home;

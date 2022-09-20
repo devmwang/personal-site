b9daedd782ae3ca43965ebc9047ca5476c861d85
Michael Wang <41971562+devmwang@users.noreply.github.com>
2022-09-19 18:57:44 -0700

    Update project section cols

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,6 +3,15 @@
 module.exports = {
     content: ["./src/**/*.{js,ts,jsx,tsx}"],
     theme: {
+        screens: {
+            'sm': '640px',
+            'md': '768px',
+            'lg': '1024px',
+            'xl': '1280px',
+            '2xl': '1536px',
+            '3xl': '1792px',
+            '4xl': '2048px',
+        },
         extend: {
             colors: {
                 'white': '#FFFEFD',

5d29543709f951e6bfcab6ad3855da66062b05d7
Michael Wang <41971562+devmwang@users.noreply.github.com>
2023-11-01 13:10:20 -0700

    gifv poc: Try diff image meta tags

--- src/app/gifv-testing/[[...slug]]/page.tsx
+++ src/app/gifv-testing/[[...slug]]/page.tsx
@@ -7,9 +7,9 @@ export const metadata = {
         type: "video.other",
         images: [
             {
-                url: "https://opencloud-api.devmwang.com/v1/files/get/clog6s2zz02az83a8z0xsn38u.gif",
+                url: "https://opencloud-api.devmwang.com/v1/files/get/clog6s2zz02az83a8z0xsn38u.mp4",
                 secureUrl:
-                    "https://opencloud-api.devmwang.com/v1/files/get/clog6s2zz02az83a8z0xsn38u.gif",
+                    "https://opencloud-api.devmwang.com/v1/files/get/clog6s2zz02az83a8z0xsn38u.mp4",
                 type: "image/gif",
                 width: 845,
                 height: 555,

3948b3dcd59b2c699d3c2fe2313eb2394b7c8e43
Michael Wang <41971562+devmwang@users.noreply.github.com>
2023-11-01 13:36:20 -0700

    gifv poc: Try other urls

--- src/app/api/oembed/route.ts
+++ src/app/api/oembed/route.ts
@@ -6,7 +6,7 @@ export async function GET(request: NextRequest) {
         type: "photo",
         width: 1920,
         height: 1080,
-        url: "https://opencloud-api.devmwang.com/v1/files/get/clog6s4sz02b083a8kx5q1e3t",
+        url: "https://opencloud-api.devmwang.com/v1/files/get/clog6s2zz02az83a8z0xsn38u.gif",
         provider_name: "OpenCloud",
         provider_url: "https://opencloud.devmwang.com/",
     });
--- src/app/gifv-testing/[[...slug]]/page.tsx
+++ src/app/gifv-testing/[[...slug]]/page.tsx
@@ -5,12 +5,12 @@ export const metadata = {
     openGraph: {
         title: "GIFV Testing",
         type: "video.other",
-        url: "https://opencloud-api.devmwang.com/v1/files/get/clog6s4sz02b083a8kx5q1e3t",
+        url: "https://opencloud-api.devmwang.com/v1/files/get/clog6s2zz02az83a8z0xsn38u.gif",
         images: [
             {
-                url: "https://opencloud-api.devmwang.com/v1/files/get/clog6s4sz02b083a8kx5q1e3t",
+                url: "https://opencloud-api.devmwang.com/v1/files/get/clog6s2zz02az83a8z0xsn38u.gif",
                 secureUrl:
-                    "https://opencloud-api.devmwang.com/v1/files/get/clog6s4sz02b083a8kx5q1e3t",
+                    "https://opencloud-api.devmwang.com/v1/files/get/clog6s2zz02az83a8z0xsn38u.gif",
                 type: "image/gif",
                 width: 845,
                 height: 555,
@@ -18,9 +18,9 @@ export const metadata = {
         ],
         videos: [
             {
-                url: "https://opencloud-api.devmwang.com/v1/files/get/clog6s4sz02b083a8kx5q1e3t",
+                url: "https://opencloud-api.devmwang.com/v1/files/get/clog6s4sz02b083a8kx5q1e3t.mp4",
                 secureUrl:
-                    "https://opencloud-api.devmwang.com/v1/files/get/clog6s4sz02b083a8kx5q1e3t",
+                    "https://opencloud-api.devmwang.com/v1/files/get/clog6s4sz02b083a8kx5q1e3t.mp4",
                 type: "video/mp4",
                 width: 845,
                 height: 555,

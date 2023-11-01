a27838609738ec50e550f9bcc63df53569a09033
Michael Wang <41971562+devmwang@users.noreply.github.com>
2023-11-01 12:14:49 -0700

    gifv poc: Update oembed api res

--- src/app/api/oembed/route.ts
+++ src/app/api/oembed/route.ts
@@ -2,10 +2,11 @@ import { NextRequest } from "next/server";
 
 export async function GET(request: NextRequest) {
     return Response.json({
-        title: "oEmbed Test",
-        type: "photo",
+        title: "oEmbed Test Video",
+        type: "video",
         height: 1440,
         width: 2560,
         version: 1.0,
+        html: '<iframe src="https://opencloud-api.devmwang.com/v1/files/get/clmuw5rg401bf83a8u6wi30jb.mp4" width="2560" height="1440" frameborder="0" title="My video" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>',
     });
 }

585eaafc741d1e0cf598361dcd51ad3f7cdd3853
Michael Wang <41971562+devmwang@users.noreply.github.com>
2023-11-01 13:29:35 -0700

    gifv poc: Reenable oembed api

--- src/app/api/oembed/route.ts
+++ src/app/api/oembed/route.ts
@@ -1,21 +1,12 @@
 import { NextRequest } from "next/server";
 
 export async function GET(request: NextRequest) {
-    // return Response.json({
-    //     title: "oEmbed Test Video",
-    //     type: "video",
-    //     height: 1080,
-    //     width: 1920,
-    //     version: 1.0,
-    //     html: '<iframe src="https://opencloud-api.devmwang.com/v1/files/get/clog52boi02af83a8azobspza.mp4" width="2560" height="1440" frameborder="0" title="My video" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>',
-    // });
-
     return Response.json({
         title: "oEmbed Test Video",
         type: "photo",
         width: 1920,
         height: 1080,
-        url: "https://opencloud-api.devmwang.com/v1/files/get/clog6s2zz02az83a8z0xsn38u.gif",
+        url: "https://opencloud-api.devmwang.com/v1/files/get/clog6s4sz02b083a8kx5q1e3t",
         provider_name: "OpenCloud",
         provider_url: "https://opencloud.devmwang.com/",
     });

e90c4d8e01c029e074ebcfb2a77589648c5868ab
Michael Wang <41971562+devmwang@users.noreply.github.com>
2023-11-01 13:44:41 -0700

    gifv poc: Other oembed api settings

--- src/app/api/oembed/route.ts
+++ src/app/api/oembed/route.ts
@@ -2,12 +2,12 @@ import { NextRequest } from "next/server";
 
 export async function GET(request: NextRequest) {
     return Response.json({
-        title: "oEmbed Test Video",
-        type: "photo",
-        width: 1920,
-        height: 1080,
-        url: "https://opencloud-api.devmwang.com/v1/files/get/clog6s2zz02az83a8z0xsn38u.gif",
         provider_name: "OpenCloud",
         provider_url: "https://opencloud.devmwang.com/",
+        title: "GIFV Testing",
+        type: "photo",
+        width: 845,
+        height: 555,
+        url: "https://opencloud-api.devmwang.com/v1/files/get/clog6s2zz02az83a8z0xsn38u.gif",
     });
 }

81fe8ebb3a62351e386d96f33ea63e1678de9471
Michael Wang <41971562+devmwang@users.noreply.github.com>
2023-11-01 13:25:41 -0700

    gif (non-video) test

---
+++ src/app/gif-testing/[[...slug]]/page.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+export const metadata = {
+    metadataBase: new URL("https://devmwang.com"),
+    openGraph: {
+        title: "GIFV Testing",
+        url: "https://opencloud-api.devmwang.com/v1/files/get/clog6s2zz02az83a8z0xsn38u",
+        images: [
+            {
+                url: "https://opencloud-api.devmwang.com/v1/files/get/clog6s2zz02az83a8z0xsn38u",
+                secureUrl:
+                    "https://opencloud-api.devmwang.com/v1/files/get/clog6s2zz02az83a8z0xsn38u",
+                type: "image/gif",
+                width: 845,
+                height: 555,
+            },
+        ],
+    },
+    robots: {
+        index: false,
+    },
+};
+
+export default function Page() {
+    return <div></div>;
+}
